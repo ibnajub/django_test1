963ae5e3ed032e1aa8dfc14204dc7740c2aa0bd0
Artem <ibnajub@gmail.com>
2023-01-21 00:50:47 +0200

    h1

--- homework/urls.py
+++ homework/urls.py
@@ -18,4 +18,21 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('acricles/', admin.site.urls),
+    path('acrticles/archive', admin.site.urls),
+    path('users/', admin.site.urls),
+
+    # http://127.0.0.1:8000/article/<int:article_number>,
+
+    # http://127.0.0.1:8000/article/<int:article_number>/archive,
+
+    # http://127.0.0.1:8000/article/<int:article_number>/<slug:slug_text>,
+
+    # http://127.0.0.1:8000/users/<int:user_number>
+
+    # Создать урл который будет принимать параметр вида 4 символа от 1 до 9, или от a до f, знак дефиса и еще 6 символов
+    # , например /34f1-1ac498/
+
+    # Создать урл который будет принимать в качестве параметра корректный номер украинского мобильного телефона,
+    # 0501231211 - корректно, 0751231212 - нет
 ]
