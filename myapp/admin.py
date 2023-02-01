5fc89b144d423b2e68e8b1bd84c992e921a9fca9
Artem <ibnajub@gmail.com>
2023-02-01 03:20:38 +0200

    models

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,18 +1,20 @@
 from django.contrib import admin
 # from .models import Article
 # import models
+from .models import Catalog_book,Catalog_author,Library_book,Library_author,Library_user_reader_history,\
+    Library_User_reader,Library_allowed_book,Artc_user,Artc_article,Artc_likes,Artc_comment
+
+admin.site.register(Catalog_author)
+admin.site.register(Catalog_book)
 #
-# admin.site.register(models.Catalog_author)
-# admin.site.register(models.Catalog_book)
-#
-# admin.site.register(models.Library_author)
-# admin.site.register(models.Library_book)
-# admin.site.register(models.Library_book_Library_author)
-# admin.site.register(models.Library_User_reader)
-# admin.site.register(models.Library_user_reader_history)
-# admin.site.register(models.Library_allowed_book)
-#
-# admin.site.register(models.Artc_user)
-# admin.site.register(models.Artc_article)
-# admin.site.register(models.Artc_likes)
-# admin.site.register(models.Artc_comment)
+admin.site.register(Library_author)
+admin.site.register(Library_book)
+
+admin.site.register(Library_User_reader)
+admin.site.register(Library_user_reader_history)
+admin.site.register(Library_allowed_book)
+
+admin.site.register(Artc_user)
+admin.site.register(Artc_article)
+admin.site.register(Artc_likes)
+admin.site.register(Artc_comment)
--- myapp/models.py
+++ myapp/models.py
@@ -10,66 +10,83 @@ class Catalog_author(models.Model):
 
 class Catalog_book(models.Model):
     name = models.CharField(max_length=100, blank=False)
-    author = models.OneToOneField(Catalog_author, on_delete=models.CASCADE, related_name='cat_book')
-#
-#
-# # 2 Разработать книжную библиотеку. Храним книги, храним авторов, книгу могут написать несколько соавторов. храним кто
-# # брал книги, и доступна ли книга сейчас.
-# class Library_author(models.Model):
-#     name = models.CharField(max_length=100, blank=False)
-#
-#
-# class Library_book(models.Model):
-#     name = models.CharField(max_length=100, blank=False)
-#
-# class Library_book_Library_author(models.Model):
-#     book = models.ForeignKey(Library_author, on_delete=models.CASCADE, related_name='l_book')
-#     author = models.ForeignKey(Library_author, on_delete=models.CASCADE, related_name='l_book')
-#
-# class Library_User_reader(models.Model):
-#     name = models.CharField(max_length=100, blank=False)
-#
-#
-# class Library_user_reader_history(models.Model):
-#     date = models.DateField(auto_now=True)
-#     user = models.ForeignKey(Library_User_reader, on_delete=models.CASCADE, related_name='l_user')
-#     book = models.ForeignKey(Library_book, on_delete=models.CASCADE, related_name='l_book')
-#     reading = models.BooleanField()
-#
-#
-# class Library_allowed_book(models.Model):
-#     book = models.ForeignKey(Library_book, on_delete=models.CASCADE, related_name='l_book')
-#     allowed = models.BooleanField()
-#
-#
+    author = models.ForeignKey(Catalog_author, on_delete=models.CASCADE, related_name='cat_book')
+
+
+# 2 Разработать книжную библиотеку. Храним книги, храним авторов, книгу могут написать несколько соавторов. храним кто
+# брал книги, и доступна ли книга сейчас.
+class Library_author(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    def __str__(self):
+        return self.name
+
+
+class Library_book(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    author = models.ManyToManyField(Library_author)
+    def __str__(self):
+        return self.name
+
+
+class Library_User_reader(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    def __str__(self):
+        return self.name
+
+
+class Library_user_reader_history(models.Model):
+    date = models.DateField(auto_now=True)
+    user = models.ForeignKey(Library_User_reader, on_delete=models.CASCADE)
+    book = models.ForeignKey(Library_book, on_delete=models.CASCADE)
+    reading = models.BooleanField()
+    def __str__(self):
+        return self.user.name
+
+
+class Library_allowed_book(models.Model):
+    book = models.ForeignKey(Library_book, on_delete=models.CASCADE)
+    allowed = models.BooleanField()
+    def __str__(self):
+        return self.book.name
+
+
 # # 3 Разработать набор моделей, для сайта-блога, на котором можно выставлять свои статьи, комментировать чужие, ставить
 # # лайк и дизлайк статье, и комментарию.
 # # 3.1 Доделать так, что бы связи позволяли комментировать комментарии.
 # # 3.2* Сделать лайки через GenericForeignKey
 #
-# class Artc_user(models.Model):
-#     name = models.CharField(max_length=100, blank=False)
-#
-#
-# class Artc_article(models.Model):
-#     date = models.DateField(auto_now=True)
-#     date_update = models.DateTimeField(auto_now_add=True)
-#     User = models.CharField(max_length=100)
-#     name = models.CharField(max_length=100)
-#     text = models.TextField(null=True, blank=True)
-#
-#
-# class Artc_likes(models.Model):
-#     article = models.CharField(max_length=100)
-#     user = models.TextField(null=True, blank=True)
-#     date = models.DateTimeField(auto_now_add=True)
-#     like_dislike = models.BooleanField()
-#
-#
-# class Artc_comment(models.Model):
-#     parrent = models.CharField(max_length=100)
-#     article = models.CharField(max_length=100)
-#     user = models.TextField(null=True, blank=True)
-#     date = models.DateField(auto_now=True)
-#     date_update = models.DateTimeField(auto_now_add=True)
-#     comment_text = models.TextField(null=True, blank=True)
+class Artc_user(models.Model):
+    name = models.CharField(max_length=100, blank=False)
+    def __str__(self):
+        return self.name
+
+
+class Artc_article(models.Model):
+    date = models.DateField(auto_now=True)
+    date_update = models.DateTimeField(auto_now_add=True)
+    header = models.CharField(max_length=100)
+    author = models.ForeignKey(Artc_user, on_delete=models.CASCADE)
+    text = models.TextField(null=True, blank=True)
+    def __str__(self):
+        return self.header
+
+
+class Artc_likes(models.Model):
+    article = models.ForeignKey(Artc_article, on_delete=models.CASCADE)
+    user = models.ForeignKey(Artc_user, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    like_dislike = models.BooleanField()
+
+    def __str__(self):
+        return self.article.header
+
+
+class Artc_comment(models.Model):
+    parrent = models.ForeignKey('myapp.Artc_comment', null=True, blank=False, on_delete=models.DO_NOTHING)
+    article = models.ForeignKey( Artc_article , on_delete=models.CASCADE)
+    user = models.ForeignKey( Artc_user , on_delete=models.CASCADE)
+    date = models.DateField(auto_now=True)
+    date_update = models.DateTimeField(auto_now_add=True)
+    comment_text = models.CharField(max_length=1000)
+    def __str__(self):
+        return self.user.name
